7f1ad98c673ff6ed5db409dcca62a96a05da5fa3
Armin Sczuka <sczuka@hotmail.com>
2019-04-11 15:13:15 +0200

    Initial version

---
+++ init.sh
@@ -0,0 +1,18 @@
+NEXUS_DATA=/srv/nexus3
+
+sudo mkdir -p $NEXUS_DATA/data/
+
+if [ ! -f $NEXUS_DATA/data/etc/nexus.properties ]; then
+   sudo cp nexus.properties $NEXUS_DATA/data/etc/nexus.properties
+fi 
+
+sudo chown -R 200:200 $NEXUS_DATA
+
+if [ ! -f server.key ]; then
+   echo "Generating private key"
+   openssl genrsa -out server.key 2048
+   echo "Generating Certificate Signing Reqeust"
+   openssl req -new -key server.key -out server.csr
+   echo "Creating certificate"
+   openssl x509 -req -in server.csr -signkey server.key -out server.crt
+fi
